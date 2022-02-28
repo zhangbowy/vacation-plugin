d5c3629afa40ba11596d9f340f7801d4ffc2451c
zhangbo <1751179889@qq.com>
2022-02-28 14:59:47 +0800

    merge: 合并冲突提交

--- src/pages/Rules/Rules.tsx
+++ src/pages/Rules/Rules.tsx
@@ -10,28 +10,153 @@ import Buttons from './components/Buttons';
 import Empty from './components/Empty';
 import AddRulePop from './components/AddRulePop';
 import './Rules.less';
+import { getRulesList, delRule, addRule } from '@/services/rules';
+import getActions from '@/components/table/getAction';
+import checkAuth from '@/utils/checkAuth';
+import { confirm } from '@/components/pop/Modal';
+import loading from '@/components/pop/loading';
+import { msg } from '@/components/pop';
+
+interface AgeRules {
+  type: string;
+  minAge: number;
+  maxAge: number;
+  quota: number;
+}
+
+interface ExpireRule {
+  expireType: string;
+  fixedTime: number;
+  fixedUnit: string;
+  nextMonthValue: string;
+  specifyDay: string;
+  untilDay: string;
+  isExtended: boolean;
+  extendedTime: string;
+  extendedUnit: string;
+  expireRemindFlag: boolean;
+  advanceRemindTime: string;
+  advanceRemindUnit: string;
+  remindScopeList: any[];
+  remindRoles: number[];
+  expireHandleType: string;
+}
+
+interface LeaveCertificate {
+  duration: number;
+  enable: boolean;
+  unit: string;
+  promptInformation: string;
+}
+
+interface QuotaRule {
+  quotaType: string;
+  fixedQuota: number;
+  ageRules: AgeRules[];
+  averageType: string;
+  roundType: string;
+  roundStep: number;
+}
+
+interface Result {
+  id: string;
+  corpId: string;
+  companyId: string;
+  vacationTypeRule: VacationTypeRule;
+  vacationIssueRule: VacationIssueRule;
+}
+
+interface RuleResult {
+  success: boolean;
+  errorCode: string;
+  errorMsg: string;
+  result: Result[];
+  arguments: any[];
+}
+
+interface SubmitTimeRule {
+  timeType: string;
+  timeUnit: string;
+  timeValue: number;
+  enableTimeLimit: boolean;
+}
+
+interface TargetRule {
+  targetType: string;
+  minAge: number;
+  maxAge: number;
+  sex: number;
+}
+
+interface TimeRule {
+  issueType: string;
+  issueTimeType: string;
+  issueDayOfMonth: number;
+  issueDayOfYear: string;
+  isEntryDayOnFirst: boolean;
+}
+
+interface VacationIssueRule {
+  timeRule: TimeRule;
+  targetRule: TargetRule;
+  quotaRule: QuotaRule;
+  expireRule: ExpireRule;
+}
+
+interface VacationTypeRule {
+  leaveCode: string;
+  bizType: string;
+  leaveName: string;
+  whenCanLeave: string;
+  visibilityRules: VisibilityRules[];
+  paidLeave: boolean;
+  leaveViewUnit: string;
+  hoursInPerDay: number;
+  leaveHourCeil: string;
+  leaveTimeCeilMinUnit: string;
+  minLeaveHour: number;
+  maxLeaveTime: number;
+  naturalDayLeave: boolean;
+  submitTimeRule: SubmitTimeRule;
+  leaveCertificate: LeaveCertificate;
+}
+
+interface VisibilityRules {
+  type: string;
+  visible: string[];
+}
 
 const defaultColumns = [
-  { title: '假期名称', dataIndex: 'userName' },
-  { title: '请假单位', dataIndex: 'jobNumber' },
-  { title: '请假计算时长方式', dataIndex: 'deptName' },
-  { title: '是否带薪', dataIndex: 'job' },
+  {
+    title: '假期名称',
+    dataIndex: 'vacationTypeRule',
+    render: (d: VacationTypeRule) => {
+      return d.leaveName || '-';
+    },
+  },
+  {
+    title: '请假单位',
+    dataIndex: 'vacationTypeRule',
+    render: (d: VacationTypeRule) => {
+      return d.leaveViewUnit || '-';
+    },
+  },
+  {
+    title: '请假计算时长方式',
+    dataIndex: 'deptName',
+    render: (d: VacationTypeRule) => {
+      return d?.naturalDayLeave ? '按工作日计算' : '-';
+    },
+  },
+  {
+    title: '是否带薪',
+    dataIndex: 'job',
+    render: (d: VacationTypeRule) => {
+      return d?.paidLeave ? '是' : '否';
+    },
+  },
   { title: '适用范围', dataIndex: 'job' },
 ];
-const getColumns = (cell?: any) => {
-  if (cell && cell[0] && cell[0].balanceDetails) {
-    return [
-      ...defaultColumns,
-      ...cell[0].balanceDetails.map(
-        ({ ruleId, ruleName, unit }: { ruleId: number; ruleName: string; unit: string }) => ({
-          title: `${ruleName}(${unit})`,
-          dataIndex: ruleId,
-        }),
-      ),
-    ];
-  }
-  return defaultColumns;
-};
 
 const Rules: FC = () => {
   const dispatch = useDispatch();
@@ -39,54 +164,84 @@ const Rules: FC = () => {
     dispatch({
       type: 'table/initTable',
       payload: {
-        name: 'balance',
-        action: getBalanceList,
+        name: 'rule',
+        action: getRulesList,
         columns: getColumns(),
         paramsHandle: (p: Record<string, any> = {}, pageNo: number, pageSize: number) => {
-          const { deptIds, ...rest } = p;
-          if (deptIds && deptIds.length > 0) {
-            rest.deptIds = deptIds.map(({ id }: { id: string | number }) => id).join(',');
-          }
+          const { ...rest } = p;
           rest.pageNo = pageNo || 1;
           rest.pageSize = pageSize || 10;
           return rest;
         },
-        resultHandle: (r: any) => {
-          if (r && r.list) {
-            getColumns(r.list);
-            return {
-              ...r,
-              ...r.list.map(
-                ({
-                  balanceDetails,
-                  ...rest
-                }: {
-                  balanceDetails: { ruleId: number; balance: number }[];
-                }) => {
-                  if (balanceDetails && Array.isArray(balanceDetails)) {
-                    const extra = {};
-                    balanceDetails.forEach(({ ruleId, balance }) => (extra[ruleId] = balance));
-                    return {
-                      ...rest,
-                      ...extra,
-                    };
-                  }
-                  return rest;
-                },
-              ),
-            };
-          }
-
-          return r;
+        resultHandle: (fetchResult: Result[]) => {
+          return {
+            list: fetchResult.map((v: any, i: number) => ({ ...v, key: i })),
+          };
         },
       },
     });
   }, [dispatch]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const onClick_edit = (d: Result) => {};
+
+  const Add = (d: Result) => {
+    const param = {};
+    addRule(param).then((res) => {});
+  };
+
+  const onClick_del = (d: Result) => {
+    confirm({
+      title: '提示',
+      content: '确定要删除假期规则吗？',
+      onOk: () => {
+        loading.show();
+        delRule({ id: d.id, leaveCode: d.vacationTypeRule.leaveCode })
+          .then(([success]) => {
+            if (success) {
+              msg('删除成功');
+              dispatch({ type: 'table/refreshTable' });
+            }
+          })
+          .finally(() => {
+            loading.hide();
+          });
+      },
+    });
+  };
+
+  const getColumns = () => {
+    const actionsReturn = getActions({
+      width: 120,
+      getHandles: (v: any) => {
+        const r = [];
+        if (checkAuth(6003)) {
+          r.push({
+            title: '编辑',
+            handle: onClick_edit,
+          });
+        }
+        if (checkAuth(6004)) {
+          r.push({
+            title: '删除',
+            handle: onClick_del,
+          });
+        }
+        return r;
+      },
+    });
+    return [...defaultColumns, actionsReturn];
+  };
+
+  const init = () => {};
+
   return (
     <PageContent className="pg-balance" hasPadding>
       <Header />
-      {/* eslint-disable-next-line no-constant-condition */}
-      {true ? (
+      {false ? (
         <Empty />
       ) : (
         <>
@@ -94,10 +249,11 @@ const Rules: FC = () => {
             {/*<Filters />*/}
             <Buttons />
           </div>
-          <StoreTable name="balance" rowKey="userId" withFooterPaination />
+          <StoreTable name="rule" rowKey="key" withFooterPaination />
           {/*<BalanceDetail visible={visible} onClose={handleCloseDetail} />*/}
         </>
       )}
+
       {/*新增假期规则弹窗*/}
       <AddRulePop />
     </PageContent>
