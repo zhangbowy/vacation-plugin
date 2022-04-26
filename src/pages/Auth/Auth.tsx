176b8e982b8a74ce8de71c7b1a4d0370327680c3
zhangbowy <1751170889@qq.com>
2022-04-26 16:32:58 +0800

    feat: 用户权限配置增加假期管理

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,100 +1,102 @@
-import { memo, useState, useEffect, useCallback } from 'react'
-import type { FC } from 'react'
-import PageContent from '@/components/structure/PageContent'
-import StoreTable from '@/components/table/StoreTable'
-import { useDispatch } from 'dva'
-import Button from '@/components/buttons/Button'
-import Icon from '@/components/Icon'
-import './Auth.less'
-import AuthEdit from './components/AuthEdit'
-import Header from './components/Header'
-import { getResourceList } from '@/services/role'
-import { getResources, getInitTablePayload } from './handleAuth'
-import { confirm } from '@/components/pop/Modal'
-import { msg } from '@/components/pop'
-import { removeRole } from '@/services/role'
-import loading from '@/components/pop/loading'
-import checkAuth from '@/utils/checkAuth'
+import { memo, useState, useEffect, useCallback } from 'react';
+import type { FC } from 'react';
+import PageContent from '@/components/structure/PageContent';
+import StoreTable from '@/components/table/StoreTable';
+import { useDispatch } from 'dva';
+import Button from '@/components/buttons/Button';
+import Icon from '@/components/Icon';
+import './Auth.less';
+import AuthEdit from './components/AuthEdit';
+import Header from './components/Header';
+import { getResourceList } from '@/services/role';
+import { getResources, getInitTablePayload } from './handleAuth';
+import { confirm } from '@/components/pop/Modal';
+import { msg } from '@/components/pop';
+import { removeRole } from '@/services/role';
+import loading from '@/components/pop/loading';
+import checkAuth from '@/utils/checkAuth';
+import { getDropdownList } from '@/services/vacationType';
 
 const Auth: FC = () => {
-  const [resourceList, setResourceList] = useState<any[] | null>(null)
-  const dispatch = useDispatch()
-  const [info, setInfo] = useState<{ visible: boolean, id: string }>({
-    visible: false, id: ''
-  })
+  const [resourceList, setResourceList] = useState<any[] | null>(null);
+  const dispatch = useDispatch();
+  const [info, setInfo] = useState<{ visible: boolean; id: string }>({
+    visible: false,
+    id: '',
+  });
+  const [ruleOptions, setRuleOptionss] = useState<OptionProps[]>([]);
   const handleEdit = (d: any) => {
-    setInfo({ visible: true, id: d.id })
-  }
+    setInfo({ visible: true, id: d.id });
+  };
   const handleRemove = useCallback(
     (d: any) => {
       confirm({
         title: '提示',
         content: '确定要删除权限吗？',
         onOk: () => {
-          loading.show()
-          removeRole({ id: d.id }).then(
-            ([success]) => {
-              loading.hide()
-              if (success) {
-                msg('删除成功')
-                dispatch({ type: 'table/refreshTable' })
-              }
+          loading.show();
+          removeRole({ id: d.id }).then(([success]) => {
+            loading.hide();
+            if (success) {
+              msg('删除成功');
+              dispatch({ type: 'table/refreshTable' });
             }
-          )
-        }
-      })
+          });
+        },
+      });
     },
-    [dispatch]
-  )
-  useEffect(
-    () => {
-      getResourceList().then(
-        ([success, result]) => {
-          const resources = success ? getResources(result || []) : []
-          setResourceList(resources)
-          dispatch({
-            type: 'table/initTable',
-            payload: getInitTablePayload(
-              resources, handleEdit, handleRemove
-            )
-          })
-        }
-      )
-    },
-    [dispatch, handleRemove]
-  )
-  useEffect(
-    () => {
-      return () => {
-        dispatch({ type: 'table/close' })
+    [dispatch],
+  );
+  useEffect(() => {
+    getResourceList().then(([success, result]) => {
+      const resources = success ? getResources(result || []) : [];
+      setResourceList(resources);
+      dispatch({
+        type: 'table/initTable',
+        payload: getInitTablePayload(resources, handleEdit, handleRemove),
+      });
+    });
+  }, [dispatch, handleRemove]);
+  useEffect(() => {
+    return () => {
+      dispatch({ type: 'table/close' });
+    };
+  }, []);
+  useEffect(() => {
+    getDropdownList({
+      type: 1,
+    }).then((d) => {
+      const [success, result] = d;
+      if (success) {
+        setRuleOptionss(
+          (result || []).map(({ id, name }: { id: string | number; name: string }) => ({
+            label: name,
+            value: id,
+          })),
+        );
       }
-    },
-    []
-  )
+    });
+  }, []);
   return (
-    <PageContent className='pg-auth' hasPadding>
+    <PageContent className="pg-auth" hasPadding>
       <Header />
-      {
-        checkAuth(6002) &&
-        <div className='pg-auth--filters'>
-          <Button
-            type="primary"
-            key="primary"
-            onClick={() => setInfo({ visible: true, id: '' })}
-          >
-            <Icon type='icon-tianjia' />
+      {checkAuth(6002) && (
+        <div className="pg-auth--filters">
+          <Button type="primary" key="primary" onClick={() => setInfo({ visible: true, id: '' })}>
+            <Icon type="icon-tianjia" />
             <span>新增权限</span>
           </Button>
         </div>
-      }
-      <StoreTable name='auth' rowKey='id' />
+      )}
+      <StoreTable name="auth" rowKey="id" />
       <AuthEdit
         {...info}
         resourceList={resourceList}
+        ruleList={ruleOptions}
         onVisibleChange={(newInfo) => setInfo(newInfo)}
       />
     </PageContent>
-  )
-}
+  );
+};
 
-export default memo(Auth)
+export default memo(Auth);
--- src/pages/Auth/components/AuthEdit/AuthEdit.tsx
+++ src/pages/Auth/components/AuthEdit/AuthEdit.tsx
@@ -7,32 +7,37 @@ import { msg, errMsg } from '@/components/pop';
 import Input from '@/components/form/Input';
 import Icon from '@/components/Icon';
 import UserSelect from '@/components/form/UserSelect';
+import Select from '@/components/form/Select';
 import Button from '@/components/buttons/Button';
 import { updateRole, getRoleDetail } from '@/services/role';
 import loading from '@/components/pop/loading';
 import CheckGroups from '../CheckGroups';
 import RangeSelect from '../RangeSelect';
 import './AuthEdit.less';
+import { Group } from '@/components/form/Radio';
+import Radio from '@/components/form/Radio/Radio';
+// import Tooltip from '@/components/pop/Tooltip/Tooltip';
 
 const rules = [
   () => ({
     validator(_rule: any, value: any) {
       if (value) {
-        const { dataAuthority, depts } = value
+        const { dataAuthority, depts } = value;
         if (dataAuthority === 2 && (!depts || !depts.length)) {
           return Promise.reject(new Error('请选择指定部门'));
         }
       }
       return Promise.resolve();
     },
-  })
-]
+  }),
+];
 
 interface AuthEditProps {
   id: string;
   visible: boolean;
   onVisibleChange: ({ id, visible }: { id: string; visible: boolean }) => void;
   resourceList: any[] | null;
+  ruleList: any[];
 }
 
 const getInitialValues = () => ({
@@ -40,10 +45,13 @@ const getInitialValues = () => ({
     depts: [],
     dataAuthority: 0,
   },
+  vacationAuthority: 0,
+  vacationIds: [],
 });
 
-const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceList }) => {
+const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceList, ruleList }) => {
   const [initialValues, setInitialValues] = useState<any>(null);
+  const [vacationAuth, setVacationAuthority] = useState<any>(0);
   const dispatch = useDispatch();
   const [form] = useForm();
   const handleVisibleChange = useCallback(
@@ -58,8 +66,8 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
     () => ({
       formatter: ({ count, maxLength }: { count: number; maxLength?: number }) => (
         <p>
-          <span className='pg-auth--edit--count'>{count}</span>
-          <span className='pg-auth--edit--max-count'>{`/${maxLength}`}</span>
+          <span className="pg-auth--edit--count">{count}</span>
+          <span className="pg-auth--edit--max-count">{`/${maxLength}`}</span>
         </p>
       ),
     }),
@@ -70,7 +78,14 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
     form
       .validateFields()
       .then((values) => {
-        const { dingUsers = [], name, range, resourceInfo } = values;
+        const {
+          dingUsers = [],
+          name,
+          range,
+          resourceInfo,
+          vacationAuthority,
+          vacationIds,
+        } = values;
         const { dataAuthority, depts } = range;
         const { isAllHandleAuthority, resourceMap = {} } = resourceInfo || {};
         const params = {
@@ -78,13 +93,13 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
           name,
           dataAuthority,
           deptIds: (depts || []).map(({ id: deptId }: { id: string | number }) => deptId),
-          dingUserIds: dingUsers.map(
-            ({ id: userId }: { id: string | number }) => userId
-          ),
+          dingUserIds: dingUsers.map(({ id: userId }: { id: string | number }) => userId),
           isAllHandleAuthority: isAllHandleAuthority || false,
           resourceIds: Object.entries(resourceMap)
             .filter((v) => v[1])
             .map(([key]) => key),
+          vacationAuthority,
+          vacationIds,
         };
         updateRole(params).then(([success]) => {
           loading.hide();
@@ -121,6 +136,8 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
               name = '',
               resourceVOS = [],
               userBOS = [],
+              vacationAuthority = 0,
+              vacationIds = [],
             } = result;
             const resourceMap = {};
             if (isAllHandleAuthority) {
@@ -148,15 +165,15 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
                 ({
                   name: userName,
                   dingUserId,
-                  avatar
+                  avatar,
                 }: {
-                  name: string,
-                  dingUserId: string | number,
-                  avatar: string
+                  name: string;
+                  dingUserId: string | number;
+                  avatar: string;
                 }) => ({
                   id: dingUserId,
                   name: userName,
-                  avatar
+                  avatar,
                 }),
               ),
               range: {
@@ -172,7 +189,10 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
                 isAllHandleAuthority,
                 resourceMap,
               },
+              vacationAuthority,
+              vacationIds,
             });
+            setVacationAuthority(vacationAuthority);
           } else {
             setInitialValues(getInitialValues());
           }
@@ -191,52 +211,45 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
     () => (
       <div>
         <Button onClick={handleVisibleChange}>取消</Button>
-        <Button type='primary' onClick={handleConfirm}>
+        <Button type="primary" onClick={handleConfirm}>
           保存
         </Button>
       </div>
     ),
     [handleVisibleChange, handleConfirm],
   );
+
+  const onChangeRulePermission = (val: any) => {
+    setVacationAuthority(val.target.value);
+  };
+
   return (
     <Drawer
-      className='pg-auth--edit'
+      className="pg-auth--edit"
       visible={visible}
       onClose={handleVisibleChange}
       title={id ? '编辑权限' : '新增权限'}
       width={600}
-      closeIcon={<Icon type='icon-guanbi' />}
+      closeIcon={<Icon type="icon-guanbi" />}
       footer={footer}
     >
-      <Form layout='vertical' form={form} initialValues={initialValues}>
-        <Item
-          label='规则名称'
-          name='name'
-          rules={[{ required: true, message: '请输入规则名称' }]}
-        >
-          <Input placeholder='请输入' showCount={showCount} maxLength={30} />
+      <Form layout="vertical" form={form} initialValues={initialValues}>
+        <Item label="规则名称" name="name" rules={[{ required: true, message: '请输入规则名称' }]}>
+          <Input placeholder="请输入" showCount={showCount} maxLength={30} />
         </Item>
-        <Item
-          label='成员'
-          name='dingUsers'
-          rules={[{ required: true, message: '请选择成员' }]}
-        >
-          <UserSelect placeholder='请选择' />
+        <Item label="成员" name="dingUsers" rules={[{ required: true, message: '请选择成员' }]}>
+          <UserSelect placeholder="请选择" />
         </Item>
-        <Item label='管理范围' name='range' rules={rules}>
+        <Item label="管理范围" name="range" rules={rules}>
           <RangeSelect />
         </Item>
         <Item
-          label='分配权限'
-          name='resourceInfo'
+          label="分配权限"
+          name="resourceInfo"
           rules={[
             () => ({
               validator(_rule, value) {
-                if (
-                  value &&
-                  value.resourceMap &&
-                  Object.values(value.resourceMap).some(Boolean)
-                ) {
+                if (value && value.resourceMap && Object.values(value.resourceMap).some(Boolean)) {
                   return Promise.resolve();
                 }
                 return Promise.reject(new Error('请勾选分配权限'));
@@ -245,6 +258,25 @@ const AuthEdit: FC<AuthEditProps> = ({ id, visible, onVisibleChange, resourceLis
           ]}
         >
           <CheckGroups resourceList={resourceList} />
+        </Item>
+        <Item label="">
+          <Item label="假期管理" name="vacationAuthority" className="com-auth--range-select m-b-10">
+            <Group onChange={onChangeRulePermission}>
+              <Radio value={0}>全部假期</Radio>
+              <Radio value={1}>部分假期</Radio>
+            </Group>
+          </Item>
+          {vacationAuth === 1 && (
+            <Item label="" name="vacationIds">
+              <Select
+                options={ruleList}
+                mode={'multiple'}
+                onChange={(e, j) => {
+                  console.log(e, j);
+                }}
+              />
+            </Item>
+          )}
         </Item>
       </Form>
     </Drawer>
